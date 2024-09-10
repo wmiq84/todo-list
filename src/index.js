3c990ddd6fd44538053e654f512b6a05f71e116c
wmiq84 <79871393+wmiq84@users.noreply.github.com>
2024-09-10 15:06:29 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import odinImage from "./odin.png";
+   
+// const image = document.createElement("img");
+// image.src = odinImage;
+   
+// document.body.appendChild(image);
+
+import "./styles.css";
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
